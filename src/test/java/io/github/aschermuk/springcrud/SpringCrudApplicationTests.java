2c92835562858a1ef4719e82a0d817eaa4415e60
Ari Schermuk <arielschermuk@gmail.com>
2024-01-19 13:02:26 -0300

    initial commit

---
+++ src/test/java/io/github/aschermuk/springcrud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aschermuk.springcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
